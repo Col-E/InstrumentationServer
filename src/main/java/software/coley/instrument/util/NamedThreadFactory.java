2fdb78842f222bb259f4524b00418d0a2db4268d
Col-E <mcoley2@gmu.edu>
2023-09-25 04:53:10 -0400

    Make thread names unique based off global counter

--- src/main/java/software/coley/instrument/util/NamedThreadFactory.java
+++ src/main/java/software/coley/instrument/util/NamedThreadFactory.java
@@ -2,9 +2,19 @@ package software.coley.instrument.util;
 
 import java.util.concurrent.ThreadFactory;
 
+/**
+ * Basic thread factory which sets thread names.
+ *
+ * @author Matt Coley
+ */
 public final class NamedThreadFactory implements ThreadFactory {
+	private static int counter = 0;
 	private final String name;
 
+	/**
+	 * @param name
+	 * 		Name to assign to thread.
+	 */
 	public NamedThreadFactory(String name) {
 		this.name = name;
 	}
@@ -13,7 +23,7 @@ public final class NamedThreadFactory implements ThreadFactory {
 	public Thread newThread(Runnable r) {
 		Thread thread = new Thread(r);
 		if (name != null)
-			thread.setName(name);
+			thread.setName(name + " " + counter++);
 		return thread;
 	}
 }
